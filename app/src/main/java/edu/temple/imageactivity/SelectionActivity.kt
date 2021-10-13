036047f7dce2798662ce089d3c540a654e839ce7
Liam <Liam@DESKTOP-BBOI67G>
2021-10-13 10:07:53 -0400

    Added French translations and basic text explaining the functionality

--- app/src/main/java/edu/temple/imageactivity/SelectionActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/SelectionActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_selection.*
 
@@ -10,21 +11,23 @@ class SelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
+        findViewById<TextView>(R.id.labelView).text = resources.getString(R.string.recyclerText)
         var dataList = mutableListOf<ImageData>()
         var  imageAdapter: ImageAdapter = ImageAdapter(this)
         recyclerView.layoutManager = GridLayoutManager(this,2)
         recyclerView.adapter = imageAdapter
         this
-        dataList.add(ImageData("Abyssinian",R.drawable.abyssinian))
-        dataList.add(ImageData("Bengal",R.drawable.bengal))
-        dataList.add(ImageData("Bombay",R.drawable.bombay))
-        dataList.add(ImageData("Maine Coon",R.drawable.maine_coon))
-        dataList.add(ImageData("Munchkin",R.drawable.munchkin))
-        dataList.add(ImageData("Persian",R.drawable.persian))
-        dataList.add(ImageData("Savannah",R.drawable.savannah))
-        dataList.add(ImageData("Scottish Fold",R.drawable.scottish_fold))
-        dataList.add(ImageData("Siamese",R.drawable.simese))
-        dataList.add(ImageData("Tabby",R.drawable.tabby))
+
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[0],R.drawable.abyssinian))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[1],R.drawable.bengal))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[2],R.drawable.bombay))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[3],R.drawable.maine_coon))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[4],R.drawable.munchkin))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[5],R.drawable.persian))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[6],R.drawable.savannah))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[7],R.drawable.scottish_fold))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[8],R.drawable.simese))
+        dataList.add(ImageData(resources.getStringArray(R.array.cats)[9],R.drawable.tabby))
 
 
         imageAdapter.setDataList(dataList)
